ec8c21d52afb6fb41ee18ef5c51659751d0a374a
kumavis <aaron@kumavis.me>
2019-02-28 13:31:02 +0800

    foe - add comments

--- src/foe.js
+++ src/foe.js
@@ -4,10 +4,17 @@ require('friend').action = doAttack
 // export out own action
 module.exports = { action: doAttack }
 
+// run at runtime
+// doAttack()
+
 function doAttack() {
   console.log('hello from your foe')
   try {
-    document.body.innerHTML = 'this page has been <important>HACKED</important>'
+    // skip if attack already succeeded
+    if (document.body.innerHTML.includes('foe')) return
+    // do attack
+    document.body.innerHTML = 'this page has been <important>HACKED</important> by foe.'
+    document.body.style.backgroundColor = 'red'
   } catch (err) {
     console.log('foe failed to modify DOM')
   }
