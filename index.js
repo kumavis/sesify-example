47f603d1144e18224467c5b1ae9d36e714b8b4bf
kumavis <aaron@kumavis.me>
2019-07-23 16:27:55 -0700

    deps - add sesify-example-format

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // load some 3rd party code
 const foe = require('foe')
 const friend = require('friend')
+const format = require('sesify-example-format')
+
+
+// document.body.innerHTML += format('hello')
+
 
 // run their code
 foe.action()
